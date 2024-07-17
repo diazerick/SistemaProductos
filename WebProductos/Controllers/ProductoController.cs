0d44b21ef5bb6d67123ef0af4beec66823b58418
erick <diazf.erick@gmail.com>
2024-07-17 00:46:35 -0600

    Agregar archivos de proyecto.

---
+++ WebProductos/Controllers/ProductoController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProductos.Datos;
+using WebProductos.Models;
+
+namespace WebProductos.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            //Declararmos una lista de productos vacia
+            List<E_Producto> productos = new List<E_Producto>();
+            try
+            {
+                //Creamos un objeto de la capa de datos
+                D_Producto datos = new D_Producto();
+                //Obtenemos la lista de productos desde la capa de datos
+                productos = datos.ObtenerTodos();
+                //Pasamos la lista de productos como MODELO a la vista
+                return View("Principal", productos);
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                return View("Principal", productos);
+            }
+        }
+
+        public ActionResult IrAgregar()
+        {
+            return View("VistaAgregar");
+        }
+
+        public ActionResult Agregar(E_Producto producto)
+        {
+            //Crear objeto de la capa de datos
+            D_Producto datos = new D_Producto();
+
+            datos.Agregar(producto);
+
+            TempData["mensaje"] = $"El producto {producto.Descripcion} se registro correctamente";
+
+            //Redirigimos a la accion Index, para que en esa accion obtengamos el modelo de la vista
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Eliminar(int idProducto)
+        {
+            try
+            {
+                D_Producto datos = new D_Producto();
+                datos.Eliminar(idProducto);
+                TempData["mensaje"] = $"El producto con ID {idProducto} se elimino";
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ObtenerParaEditar(int idProducto)
+        {
+            try
+            {
+                //Crear objeto de la capa de datos
+                D_Producto datos = new D_Producto();
+                //Obtener los datos del producto
+                E_Producto producto = datos.ObtenerPorId(idProducto);
+                //Mandamos a la vista Editar con el producto como modelo
+                return View("VistaEditar", producto);
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Editar(E_Producto objProducto)
+        {
+            try
+            {
+                D_Producto datos = new D_Producto();
+
+                datos.Editar(objProducto);
+
+                TempData["mensaje"] = $"El producto con id {objProducto.IdProducto} se edito";
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebProductos/Datos/D_Producto.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebProductos.Models;
+
+namespace WebProductos.Datos
+{
+    public class D_Producto
+    {
+        private string cadenaConexion = "server=localhost;database=Generacion28;user=sa;password=devo123";
+
+        public List<E_Producto> ObtenerTodos()
+        {
+            //Creamos lista para almacenar los productos
+            List<E_Producto> lista = new List<E_Producto>();
+            //Cadena de conexion para conectarnos a la BD
+            //Objeto de la clase SqlConnecion para conectarnos a la BD
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+            try
+            {
+                //Abrir la conexion
+                conexion.Open();
+                //query a ejecutar
+                string query = "SELECT idProducto,descripcion,precio,fechaIngreso,disponible FROM Productos";
+                //Objeto de la clase SqlCommand para ejecutar el query
+                SqlCommand comando = new SqlCommand(query, conexion);
+                //Ejecutamos el query y el resultado lo almacenamos en un objeto SqlDataReader
+                SqlDataReader reader = comando.ExecuteReader();
+
+                //Recorrer el reader con los resultados del query
+                while (reader.Read())
+                {
+                    //Crear un producto y le asignamos valores a sus propiedades
+                    E_Producto producto = new E_Producto();
+                    producto.IdProducto = Convert.ToInt32(reader["idProducto"]);
+                    producto.Descripcion = reader["descripcion"].ToString();
+                    producto.Precio = Convert.ToDecimal(reader["precio"]);
+                    producto.FechaIngreso = Convert.ToDateTime(reader["fechaIngreso"]);
+                    producto.Disponible = Convert.ToBoolean(reader["disponible"]);
+                    //Agregar el producto a la lista
+                    lista.Add(producto);
+                }
+            }
+            catch (Exception ex)
+            {
+                //Cuando ocurre un error en la capa de datos
+                //Lanzamos nuevamente la excepcion para que sea controlada por try-catch del controlador
+                throw ex;
+            }
+            finally
+            {
+                //Cerrar la conexion
+                conexion.Close();
+            }
+            return lista;
+        }
+
+        public void Agregar(E_Producto objProducto)
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+            conexion.Open();
+
+            //Declarar el query utilizando parametros
+            string query = "INSERT INTO Productos(descripcion,precio,fechaIngreso,disponible) " +
+                                         "VALUES(@descripcion,@precio,@fechaIngreso,@disponible)";
+
+            //Objeto SqlCommand para ejecutar el query
+            SqlCommand comando = new SqlCommand(query, conexion);
+
+            //Asignarle valores a los parametros del query
+            comando.Parameters.AddWithValue("@descripcion", objProducto.Descripcion);
+            comando.Parameters.AddWithValue("@precio", objProducto.Precio);
+            comando.Parameters.AddWithValue("@fechaIngreso", objProducto.FechaIngreso);
+            comando.Parameters.AddWithValue("@disponible", objProducto.Disponible);
+
+            comando.ExecuteNonQuery();
+
+            conexion.Close();
+        }
+
+        public void Eliminar(int idProducto)
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+            try
+            {
+                conexion.Open();
+                string query = "DELETE Productos WHERE idProducto = @idProducto";
+
+                SqlCommand comando = new SqlCommand(query,conexion);
+
+                comando.Parameters.AddWithValue("@idProducto", idProducto);
+
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        public E_Producto ObtenerPorId(int idProducto)
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+            E_Producto producto = new E_Producto();
+            try
+            {
+                conexion.Open();
+
+                string query = "SELECT idProducto,descripcion,precio,fechaIngreso,disponible FROM Productos " +
+                                "WHERE idProducto = @idProducto";
+
+                SqlCommand comando = new SqlCommand(query,conexion);
+                comando.Parameters.AddWithValue("@idProducto", idProducto);
+
+                SqlDataReader reader = comando.ExecuteReader();
+                //Si el query nos devolvio un resultado, lo leemos
+                if (reader.Read())
+                {
+                    producto.IdProducto = Convert.ToInt32(reader["idProducto"]);
+                    producto.Descripcion = reader["descripcion"].ToString();
+                    producto.Precio = Convert.ToDecimal(reader["precio"]);
+                    producto.FechaIngreso = Convert.ToDateTime(reader["fechaIngreso"]);
+                    producto.Disponible = Convert.ToBoolean(reader["disponible"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+            return producto;
+        }
+
+        public void Editar(E_Producto producto)
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+            try
+            {
+                conexion.Open();
+
+                string query = "UPDATE Productos SET descripcion=@descripcion, precio=@precio," +
+                               "fechaIngreso=@fechaIngreso, disponible=@disponible " +
+                               "WHERE idProducto = @idProducto";
+
+                SqlCommand comando = new SqlCommand(query, conexion);
+
+                comando.Parameters.AddWithValue("@descripcion", producto.Descripcion);
+                comando.Parameters.AddWithValue("@precio", producto.Precio);
+                comando.Parameters.AddWithValue("@fechaIngreso", producto.FechaIngreso);
+                comando.Parameters.AddWithValue("@disponible", producto.Disponible);
+                comando.Parameters.AddWithValue("@idProducto", producto.IdProducto);
+
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+
+        }
+
+    }
+}
